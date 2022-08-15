010d00f7bba6be53fa21a83a7c33d942ccd91656
Md Nafiz Imtiaz Saimon <nafiz.imtiaz.saimon@gmail.com>
2022-08-15 19:55:09 +0600

    Query Service Complete

--- query/index.js
+++ query/index.js
@@ -1,14 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { restart } = require("nodemon");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/posts", (req, res) => {});
+const posts = {};
 
-app.post("/events", async (req, res) => {});
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId } = data;
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  console.log(posts);
+  res.send({});
+});
 
 app.listen(4002, () => {
   console.log("Listening on 4002");
