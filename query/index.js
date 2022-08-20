80deb098f9a407d8e90b479c4537625c01f08bd8
Md Nafiz Imtiaz Saimon <nafiz.imtiaz.saimon@gmail.com>
2022-08-20 22:55:51 +0600

    Adding Comment Moderation

--- query/index.js
+++ query/index.js
@@ -22,9 +22,10 @@ app.post("/events", async (req, res) => {
   }
 
   if (type === "CommentCreated") {
-    const { id, content, postId } = data;
+    const { id, content, postId, status } = data;
+
     const post = posts[postId];
-    post.comments.push({ id, content });
+    post.comments.push({ id, content, status });
   }
 
   console.log(posts);
