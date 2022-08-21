6e764a99bb5e5f1871896c9e84f2248256e2da32
Md Nafiz Imtiaz Saimon <nafiz.imtiaz.saimon@gmail.com>
2022-08-21 22:13:36 +0600

    Handling Moderation

--- moderation/index.js
+++ moderation/index.js
@@ -5,7 +5,23 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
-app.post("/posts", async (req, res) => {});
+app.post("/events", async (req, res) => {
+  const { type, data} = req.body;
+
+  if(type === 'CommentCreated') {
+    const status = data.content.includes('orange') ? 'rejected' : 'approved'
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        id: data.id,
+        postId: data.post.Id,
+        status,
+        content: data.content
+      }
+    })
+  }
+  res.send({})
+});
 
 app.listen(4003, () => {
   console.log("Listening on 4003");
